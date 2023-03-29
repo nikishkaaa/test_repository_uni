aafd06538343b9223ede473f9e18a0aee70466b8
Daria Nikishkina <115469161+nikishkaaa@users.noreply.github.com>
2023-03-29 16:22:04 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  lab 4 task 1
+//
+//  Created by Дарья Никишкина on 09.11.2021.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{ int a,b,s=0;
+cout<<"input a"<<endl;
+cin>>a;
+cout<<"input b"<<endl;
+cin>>b;
+for (int i=a; i<=b;i++)
+s=s+i;
+    cout<<s<<endl;
+    return 0;
+}
